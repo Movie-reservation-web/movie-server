b704c4f67aa635cb4028f84a50b8f20ed81dd3b5
greeng00se <thegreengoose7@gmail.com>
2022-03-12 17:15:36 +0900

    test: 상영관 조회 테스트 @Commit 제거

--- src/test/java/study/movie/domain/screen/ScreenRepositoryTest.java
+++ src/test/java/study/movie/domain/screen/ScreenRepositoryTest.java
@@ -4,14 +4,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.movie.domain.theather.Theater;
 import study.movie.domain.theather.TheaterRepository;
 
 @SpringBootTest
 @Transactional
-@Commit
 class ScreenRepositoryTest {
 
     @Autowired

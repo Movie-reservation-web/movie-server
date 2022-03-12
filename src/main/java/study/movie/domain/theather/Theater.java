9baca0d514ab97066197432aa0ac9771490bf034
greeng00se <thegreengoose7@gmail.com>
2022-03-12 16:19:32 +0900

    feat: Theater Entity 추가

---
+++ src/main/java/study/movie/domain/theather/Theater.java
@@ -0,0 +1,40 @@
+package study.movie.domain.theather;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import study.movie.domain.BaseTimeEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class Theater extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "theater_id")
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Builder
+    public Theater(String city, String name, String phone) {
+        this.city = city;
+        this.name = name;
+        this.phone = phone;
+    }
+}

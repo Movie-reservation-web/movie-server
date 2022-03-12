63e693f69174d71e35e558397e6d4f2f4523351c
Leeky0615 <rbdus7174@naver.com>
2022-03-12 20:24:42 +0900

    feat: test enum mapper class

---
+++ src/test/java/study/movie/global/enumMapper/EnumMapperTest.java
@@ -0,0 +1,47 @@
+package study.movie.global.enumMapper;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+class EnumMapperTest {
+
+    @Test
+    void EnumMapperValue_json형태로_리턴() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        for (TestGenre value : TestGenre.values()) {
+            String jsonDto = objectMapper.writeValueAsString(value);
+            System.out.println("jsonDto = " + jsonDto);
+        }
+
+    }
+
+    @JsonFormat(shape = JsonFormat.Shape.OBJECT)
+    enum TestGenre implements EnumMapperType {
+        ACTION("ACT","액션"),
+        ADVENTURE("ADV","모험"),
+        COMEDY("COM","코미디"),
+        DRAMA("DRA","드라마"),
+        FANTASY("FAN","판타지");
+        private String code;
+        private String desc;
+
+        TestGenre(String code, String desc) {
+            this.code = code;
+            this.desc = desc;
+        }
+
+        @Override
+        public String getCode() {
+            return code;
+        }
+
+        @Override
+        public String getDesc() {
+            return desc;
+        }
+    }
+
+
+}

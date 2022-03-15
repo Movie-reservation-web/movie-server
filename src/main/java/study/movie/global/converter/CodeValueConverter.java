769118ce5b284f4c48d5eca9be9ed3d6083fc4aa
Leeky0615 <rbdus7174@naver.com>
2022-03-15 16:25:25 +0900

    chore: add comment

--- src/main/java/study/movie/global/converter/CodeValueConverter.java
+++ src/main/java/study/movie/global/converter/CodeValueConverter.java
@@ -1,8 +1,6 @@
 package study.movie.global.converter;
 
-import study.movie.global.constants.EntityAttrConst;
 import study.movie.global.enumMapper.EnumMapperType;
-import study.movie.global.enumMapper.EnumMapperValue;
 
 import javax.persistence.AttributeConverter;
 import java.util.EnumSet;
@@ -13,15 +11,29 @@ public class CodeValueConverter<E extends Enum<E> & EnumMapperType> implements A
 
     private Class<E> clazz;
 
+    /**
+     * type parameter 로 들어온 값을 선언
+     * @param enumClazz
+     */
     public CodeValueConverter(Class<E> enumClazz) {
         this.clazz = enumClazz;
     }
 
+    /**
+     * Enum 의 code 값을 리턴
+     * @param attribute
+     * @return String
+     */
     @Override
     public String convertToDatabaseColumn(E attribute) {
         return attribute.getCode();
     }
 
+    /**
+     * 위에서 선언한 type parameter(enum class)에서 parameter 에 해당하는 값을 찾아 리턴
+     * @param dbData
+     * @return Enum
+     */
     @Override
     public E convertToEntityAttribute(String dbData) {
         return EnumSet.allOf(clazz).stream()
--- src/main/java/study/movie/global/converter/EnumArrayConverter.java
+++ src/main/java/study/movie/global/converter/EnumArrayConverter.java
@@ -21,16 +21,32 @@ public class EnumArrayConverter<E extends Enum<E> & EnumMapperType> implements A
         this.clazz = enumClazz;
     }
 
+    /**
+     * List 로 들어온 파라미터에 대해 code값을 mapping 시켜 구분자로 연결
+     * @param attribute
+     * @return String
+     */
     @Override
     public String convertToDatabaseColumn(List<E> attribute) {
         return attribute.stream().map(E::getCode).collect(Collectors.joining(SEPARATOR));
     }
 
+    /**
+     * DB 에서 들어온 데이터르 구분자(,)로 나눈 뒤 값을 convertToEnum 메소드로 mapping 시킨 후
+     * 값을 리스트에 담아 리턴
+     * @param dbData
+     * @return List<Enum>
+     */
     @Override
     public List<E> convertToEntityAttribute(String dbData) {
         return Arrays.stream(dbData.split(SEPARATOR)).map(this::convertToEnum).collect(Collectors.toList());
     }
 
+    /**
+     * 위에서 선언한 type parameter(enum class)에서 parameter 에 해당하는 값을 찾아 리턴
+     * @param data
+     * @return Enum
+     */
     private E convertToEnum(String data) {
         return EnumSet.allOf(clazz).stream()
                 .filter(e -> e.getCode().equals(data))

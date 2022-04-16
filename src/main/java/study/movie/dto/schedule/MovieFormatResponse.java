08489971fd95778c1b039053cf0cf8468dbca2c8
Leeky0615 <rbdus7174@naver.com>
2022-04-16 17:00:15 +0900

    fix: edit schedule search logic

---
+++ src/main/java/study/movie/dto/schedule/MovieFormatResponse.java
@@ -0,0 +1,18 @@
+package study.movie.dto.schedule;
+
+import lombok.Data;
+import study.movie.domain.theater.ScreenFormat;
+
+import java.util.List;
+
+@Data
+public class MovieFormatResponse {
+
+    private String movieTitle;
+    private List<ScreenFormat> formats;
+
+    public MovieFormatResponse(String movieTitle, List<ScreenFormat> formats) {
+        this.movieTitle = movieTitle;
+        this.formats = formats;
+    }
+}

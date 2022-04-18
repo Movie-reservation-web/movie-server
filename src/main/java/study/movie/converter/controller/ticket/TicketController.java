f3a476b05861eb22da2dee5326c10fd8ec6635d4
Leeky0615 <rbdus7174@naver.com>
2022-04-18 17:39:46 +0900

    feature/ticket [Domain] Ticket Domain 개발

---
+++ src/main/java/study/movie/converter/controller/ticket/TicketController.java
@@ -0,0 +1,14 @@
+package study.movie.converter.controller.ticket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.movie.global.controller.BasicController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/tickets")
+public class TicketController extends BasicController {
+
+
+}

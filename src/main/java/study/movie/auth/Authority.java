4962ea5bee571bc13d2520c479df39fee114b136
KimKyungHyunn <kdkin00@gmail.com>
2022-05-24 14:51:22 +0900

    feat: member, auth, redis (실행 오류로 삭제해둠)

--- src/main/java/study/movie/auth/Authority.java
+++
@@ -1,9 +0,0 @@
-package study.movie.auth;
-
-public enum Authority {
-    ADMIN, USER;
-
-    public String getRoles(){
-        return "ROLE_" + name();
-    }
-}
--- src/main/java/study/movie/member/service/MemberService.java
+++
@@ -1,8 +0,0 @@
-package study.movie.member.service;
-
-import study.movie.member.dto.request.LoginRequest;
-import study.movie.member.dto.response.LoginResponse;
-
-public interface MemberService {
-    LoginResponse login(LoginRequest request);
-}
